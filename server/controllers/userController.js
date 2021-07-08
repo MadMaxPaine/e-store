e0cb63371b223f86198de563408e7feeefef98f9
Олександр Холод <workkholod@gmail.com>
2021-07-08 16:45:31 +0300

    Basic AAA

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,51 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+const { User, Basket } = require('../models/models');
 const ApiError = require('../error/ApiErrors');
+
+const generrateJwt = (id, email, role) => {
+ return jwt.sign({ id, email, role },
+  process.env.SECRET_KEY,
+  {
+   expiresIn: '24h'
+  });
+
+}
+
 class UserController {
- async registration(req, res) {
-
+ async registration(req, res, next) {
+  const { email, password, role } = req.body;
+  if (!email || !password) {
+   next(ApiError.badRequest('Wrong email or password'));
+  }
+  const candidate = await User.findOne({ where: { email } });
+  if (candidate) {
+   next(ApiError.badRequest('User with current e-mail exists!'));
+  }
+  const hashPassword = await bcrypt.hash(password, 5);
+  const user = await User.create({ email, role, password: hashPassword });
+  const basket = await Basket.create({ userId: user.id });
+  const token = generrateJwt(user.id, user.email, user.role);
+  return res.json({ token });
  }
- async login(req, res) {
-
+ async login(req, res, next) {
+  const { email, password } = req.body;
+  const user = await User.findOne({ where: { email } });
+  if (!user) {
+   next(ApiError.internal('User not found!'));
+  }
+  let comparePassword = bcrypt.compareSync(password, user.password);
+  if (!comparePassword) {
+   next(ApiError.internal('Wrong password'));
+  }
+  const token = generrateJwt(user.id, user.email, user.role);
+  return res.json({ token });
  }
- async check(req, res, next) {  
-  const { id } = req.query;
-  if (!id) { return next(ApiError.badRequest("No Id")); }
-  res.json({ message: id });
+ async check(req, res, next) {
+  const token = generrateJwt(req.user.id, req.user.email, req.user.role);
+  return res.json({ token });
  }
 }
 
--- server/models/models.js
+++ server/models/models.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequlize = require('../db');
 
 const User = sequlize.define('user', {
- user_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+ id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
  email: { type: DataTypes.STRING, unique: true },
  password: { type: DataTypes.STRING },
  role: { type: DataTypes.STRING, defaultValue: 'USER' }
