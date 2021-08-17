fb51d5288d26268d118399a86630a410ec4e9ddf
Олександр Холод <workkholod@gmail.com>
2021-08-17 04:06:02 +0300

    device history

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Card, Col, Image } from 'react-bootstrap';
 import star from '../assets/star.png';
 import { useHistory } from 'react-router-dom';
+import { DEVICE_ROUTE } from '../utils/consts';
 export const DeviceItem = ({ device }) => {
  const history = useHistory();
- console.log(history);
  return (
-  <Col className="mt-2 d-flex" md={3}>
+  <Col className="mt-2 d-flex" md={3} onClick={() => history.push(`${DEVICE_ROUTE}/${device.id}`)}>
    <Card style={{ width: 150, cursor: 'pointer' }} border={"light"}>
     <Image style={{ 'padding': 5 }} width={150} height={150} src={device.img}></Image>
     <div className="text-black-50 mt-1 d-flex justify-content-between align-items-center">
