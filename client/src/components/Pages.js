3a520278014bd96fe0daffc7e070e94026a23b21
Олександр Холод <workkholod@gmail.com>
2021-08-23 14:06:29 +0300

    pagination fast end/foreward

--- client/src/components/Pages.js
+++ client/src/components/Pages.js
@@ -4,25 +4,62 @@ import { Pagination } from 'react-bootstrap';
 import { ctx } from '../index';
 
 export const Pages = observer(() => {
- const { device } = useContext(ctx);
- const pagesCount = Math.ceil(device.totalCount / device.limit);
- const pages = [];
- for (let index = 0; index < pagesCount; index++) {
-  pages.push(index + 1);
- }
- return (
-  <Pagination className="mt-4">
-   {
-    pages.map(page =>
-     <Pagination.Item
-      activeLabel={false}
-      key={page}
-      active={device.page === page}
-      onClick={() => device.setPage(page)}
-     >
-      {page}
-     </Pagination.Item>)
-   }
-  </Pagination>
- )
+  const { device } = useContext(ctx);
+  const pagesCount = Math.ceil(device.totalCount / device.limit);
+  const pages = [];
+  for (let index = 0; index < pagesCount; index++) {
+    pages.push(index + 1);
+  }
+  return (
+    <Pagination className="d-flex mt-4" >
+      <Pagination.Item
+        className="m-1"
+        activeLabel={false}
+        disabled={pages.length <= 0 ? true : false}
+        onClick={() => device.setPage(pages[0])}
+      >{'«'}
+      </Pagination.Item>
+      <Pagination.Item
+        className="m-1"
+        activeLabel={false}
+        disabled={pages.length <= 0 ? true : false}
+        onClick={() =>
+          device.page.index > 0 ?
+            device.setPage(pages[device.page.index - 1])
+            :
+            device.setPage(pages[0])}
+      >{'‹'}
+      </Pagination.Item>
+      {
+        pages.map(page =>
+          <Pagination.Item
+            className="m-1"
+            activeLabel={false}
+            key={page}
+            active={device.page === page}
+            onClick={() => device.setPage(page)}
+          >
+            {page}
+          </Pagination.Item>)
+      }
+      <Pagination.Item
+        className="m-1"
+        activeLabel={false}
+        disabled={pages.length <= 0 ? true : false}
+        onClick={() =>
+          device.page.index < pages.length ?
+            device.setPage(pages[device.page.index + 1])
+            :
+            device.setPage(pages[pages.length - 1])}
+      >{'›'}
+      </Pagination.Item>
+      <Pagination.Item
+        className="m-1"
+        activeLabel={false}
+        disabled={pages.length <= 0 ? true : false}
+        onClick={() => device.setPage(pages[pages.length - 1])}
+      >{'»'}
+      </Pagination.Item>
+    </Pagination>
+  )
 });
