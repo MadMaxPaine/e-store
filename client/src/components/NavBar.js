ddfe6db1ba5fdc10ae883c90a35bd72eef5fe1c6
Олександр Холод <workkholod@gmail.com>
2021-09-15 13:51:50 +0300

    change loggin menu to dropdown

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,6 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
 import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import { observer } from 'mobx-react-lite';
 import { useHistory } from 'react-router-dom';
@@ -11,24 +12,26 @@ import { useHistory } from 'react-router-dom';
 const NavBar = observer(() => {
   const { user } = useContext(ctx);
   const history = useHistory();
-
   const logOut = () => {
     user.setUser({});
     user.setIsAuth(false);
   }
   return (
-    <Navbar bg="primary" variant="dark">
+    <Navbar bg="primary" variant="primary" >
       <Container>
-        <Nav.Link className="text-white" href={SHOP_ROUTE}>E-store</Nav.Link>
+        <Nav.Link className="text-white" onClick={() => history.push(SHOP_ROUTE)}>E-store</Nav.Link>
         {
           user.isAuth ?
-            <Nav className="al-auto" >
-              <Button onClick={() => history.push(ADMIN_ROUTE)}>Admin</Button>
-              <Button onClick={() => logOut()} style={{ "marginLeft": "10px" }}>Exit</Button>
-            </Nav> :
-            <Nav className="al-auto">
-              <Button onClick={() => history.push(LOGIN_ROUTE)}>Authorization</Button>
-            </Nav>
+            <Dropdown align="end" className="al-auto border rounded" style={{ padding: "1px" }}>
+              <Button size="sm" variant="primary" onClick={() => logOut()} style={{ borderRight: "1px solid white" }}>Log out</Button>
+              <Dropdown.Toggle size="sm" variant="primary" style={{ marginLeft: "1px" }}/>
+              <Dropdown.Menu align="end">
+                <Dropdown.Item onClick={() => history.push(SHOP_ROUTE)}>Store</Dropdown.Item>
+                <Dropdown.Item onClick={() => history.push(ADMIN_ROUTE)}>Admin panel</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown> :
+            <Button size="sm" variant="primary" className="al-auto border rounded" onClick={() => history.push(LOGIN_ROUTE)} >Sing in</Button>
         }
       </Container>
     </Navbar>
