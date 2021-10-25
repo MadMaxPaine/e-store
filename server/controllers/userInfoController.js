f5586d2bb1e36a55ba4639e975fa3bb8ffb18e01
Олександр Холод <workkholod@gmail.com>
2021-10-25 15:04:09 +0300

    Reworked middleware checkRole, not it's use APiError to handle errors and get Role From userDTO

--- server/controllers/userInfoController.js
+++ server/controllers/userInfoController.js
@@ -1,10 +1,10 @@
-const { registration, login, logout, activate, refresh, getUsers, check } = require('../services/userService');
-class UserInfoController {
- constructor() {
-  this.create = create;
-  this.getOne = getOne;
-  this.getAll = getAll;
+// const { registration, login, logout, activate, refresh, getUsers, check } = require('../services/userService');
+// class UserInfoController {
+//  constructor() {
+//   this.create = create;
+//   this.getOne = getOne;
+//   this.getAll = getAll;
 
- }
-}
-module.exports = new UserController();
+//  }
+// }
+// module.exports = new UserController();
