88dcf1aaf13e5a32898b6c168882a5f39f7d4d56
Олександр Холод <workkholod@gmail.com>
2021-10-26 19:51:28 +0300

    Fix user store and add isLoading to it

--- client/src/store/UserStore.js
+++ client/src/store/UserStore.js
@@ -1,11 +1,11 @@
 import { makeAutoObservable } from 'mobx';
 import axios from 'axios';
 const { login, registration, logout } = require('../http/userAPI');
-
 export default class UserStore {
  constructor() {
   this._isAuth = false;
   this._user = {};
+  this._isLoading = false;
   makeAutoObservable(this);
  }
 
@@ -15,6 +15,9 @@ export default class UserStore {
  setUser(objUser) {
   this._user = objUser;
  }
+ setLoading(bool) {
+  this._isLoading = bool;
+ }
 
  get isAuth() {
   return this._isAuth;
@@ -22,7 +25,9 @@ export default class UserStore {
  get User() {
   return this._user;
  }
-
+ get isLoading() {
+  return this._isLoading;
+ }
  async login(email, password) {
   try {
    const res = await login(email, password);
@@ -38,7 +43,6 @@ export default class UserStore {
  async registration(email, password) {
   try {
    const res = await registration(email, password);
-   console.log(res);
    localStorage.setItem('token', res.data.accessToken);
    this.setIsAuth(true);
    this.setUser(res.data.user);
@@ -51,22 +55,25 @@ export default class UserStore {
   try {
    await logout();
    localStorage.removeItem('token');
-   this.setIsAuth(false);
    this.setUser({});
+   this.setIsAuth(false);
   } catch (e) {
    console.log(e.response?.data?.message);
   }
  }
 
  async checkAuth() {
+  this.setLoading(true);
   try {
    const res = await axios.get(`${process.env.REACT_APP_API_URL}api/user/refresh`, { withCredentials: true });
-   console.log(res);
    localStorage.setItem('token', res.data.accessToken);
    this.setIsAuth(true);
    this.setUser(res.data.user);
   } catch (e) {
    console.log(e);
+  }
+  finally {
+   this.setLoading(false);
   }
  }
 }
