bab481cbeff513bd8aded468fd73c2dee3eb9a63
Олександр Холод <workkholod@gmail.com>
2022-02-13 04:41:09 +0200

    Add fix to not existing db
    test throwmysql2 connection to db and if it donot exists create a brand new one to work with

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const fileUpload = require('express-fileupload');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-
+const mysql = require('mysql2/promise');
 const sequelize = require('./db');
 const model = require('./models/models');
 const { parse } = require('dotenv');
@@ -13,6 +13,8 @@ const errorHandler = require('./middleware/errorHandlingMiddleware');
 
 const app = express();
 const PORT = process.env.PORT || 7000;
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, 'static')));
@@ -25,7 +27,7 @@ app.use('/api', router);
 
 
 app.get('/', (req, res) => {
- res.status(200).json({ message: 'Working!!!' });
+ res.status(200).json({ message: 'Server is online!' });
 });
 
 //Err handler. Must be in the end because have no next inside
@@ -33,6 +35,15 @@ app.use(errorHandler);
 
 const start = async () => {
  try {
+  //Try if db exists and addnew one if it doesn't
+  const connection = await mysql.createConnection(
+   {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+   });
+  await connection.query(`CREATE DATABASE IF NOT EXISTS \`e-store\`;`);
   await sequelize.authenticate();
   await sequelize.sync();
   app.listen(PORT, () => console.log(`Server is starting on port: ${PORT}`));
