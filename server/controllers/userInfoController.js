1963e6223b2bd6631cc8f240e1672e434f97ad08
Олександр Холод <workkholod@gmail.com>
2022-07-19 20:16:04 +0300

    userInfoController basic setup
    basic controller setup

--- server/controllers/userInfoController.js
+++ server/controllers/userInfoController.js
@@ -1,10 +1,7 @@
-// const { registration, login, logout, activate, refresh, getUsers, check } = require('../services/userService');
-// class UserInfoController {
-//  constructor() {
-//   this.create = create;
-//   this.getOne = getOne;
-//   this.getAll = getAll;
-
-//  }
-// }
-// module.exports = new UserController();
+const { create } = require('../services/userInfoService');
+class UserInfoController {
+ constructor() {
+  this.create = create;  
+ }
+}
+module.exports = new UserInfoController();
