37958d1a68b844f46b509d6c764c8efbf895e6da
Олександр Холод <workkholod@gmail.com>
2022-07-19 20:13:05 +0300

    fixing models User info and general models.js
    minor fix

--- server/models/models.js
+++ server/models/models.js
@@ -69,6 +69,7 @@ module.exports = {
  Type, Rating,
  TypeBrand,
  DeviceInfo,
- Token
+ Token,
+ UserInfo
 };
 
--- server/models/userInfoModel.js
+++ server/models/userInfoModel.js
@@ -5,7 +5,6 @@ const UserInfo = sequlize.define('user_info', {
  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
  firstName: { type: DataTypes.STRING, allowNull: false },
  secondName: { type: DataTypes.STRING, allowNull: false },
- age: { type: DataTypes.INTEGER },
  gender: { type: DataTypes.STRING },
  avatar: { type: DataTypes.STRING },
  phone: { type: DataTypes.STRING, allowNull: false }
